3fd1cd79aeea906d003dc1ddc6155826a31604f9
Nadia Virjee <nadvirjee@gmail.com>
2022-12-15 16:34:37 -0500

    commit tree 1749c5b4896accd0bd3fbcfe11cf2a1d836cd0e8

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+    name: "Nadia Virjee",
+    streetAddress: "1796 Northampton Drive",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    const deleteEmployee = {...delete employee.value};
+    deleteEmployee[key] = value;
+    return deleteEmployee
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    employee[key] = value;
+    delete employee.value
+    return employee
+};
